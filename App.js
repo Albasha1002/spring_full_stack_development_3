f3214c3d7f00e9aeb87c6a54aabeb65df8b70394
Al Shagoule Hamedou Bathousha <78254037+Albasha1002@users.noreply.github.com>
2023-08-28 20:53:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import AddEmployee from './components/AddEmployee';
+
+function App() {
+  return (
+ //fragment
+  <>
+    <Navbar />
+    <AddEmployee />
+  </>
+  );
+  
+}
+
+export default App;
